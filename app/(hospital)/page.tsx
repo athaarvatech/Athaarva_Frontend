c62c12eb131e02f71aedceddbd089b3569a2fc3b
Mannan-Tyagi <mannan.tyagi@gmail.com>
2026-01-29 22:14:34 +0530

    wb-fixed

--- app/(hospital)/page.tsx
+++ app/(hospital)/page.tsx
@@ -1,4 +1,7 @@
+import type { Metadata } from "next";
 import HospitalHomeClient from "./HospitalHomeClient";
+
+export const dynamic = "force-dynamic";
 
 export default function HospitalHomePage() {
   return <HospitalHomeClient />;
